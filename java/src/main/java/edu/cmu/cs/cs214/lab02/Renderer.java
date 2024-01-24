31b4bffa3b163fab22cbc46c24c25013a68323ce
ZAN <zanorz233@gmail.com>
2024-01-23 23:15:19 -0500

    Update Renderer to use Shape interface for flexibility

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,14 +3,14 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    private Shape shape;
+
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
-    void draw() {
-        double area = rectangle.getArea();
+    public void draw() {
+        double area = shape.getArea();
 
         // assume implementation
 
