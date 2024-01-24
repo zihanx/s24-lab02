d302a40f6926a1e001c9d668ae2dee8c469f0cb3
ZAN <zanorz233@gmail.com>
2024-01-23 23:16:13 -0500

    Refactor Main class to achieve Shape polymorphism

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,13 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape shape = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer = new Renderer(shape);
 
         renderer.draw();
     }
